dc6b7726b482369c2b41425b13d3ab54fb7f0b31
chic-page-6502 <zacharytq@gmail.com>
2019-07-13 01:28:33 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,12 @@
 # Write your code here.
+def line(deli)
+  if deli.length == 0
+    puts "The line is currently empty."
+  else
+    line = "The line is currently: "
+    deli.each_with_index do |person, index|
+      line << "#{index + 1}. person"
+    end
+    puts line
+  end
+end
