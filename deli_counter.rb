91b4eece2888b480985603931834dd7d3a729e77
chic-page-6502 <zacharytq@gmail.com>
2019-07-13 01:38:35 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -15,3 +15,11 @@ def take_a_number(deli, name)
   deli << name
   puts "Welcome, #{name}. You are number #{deli.find_index(name) + 1} in line."
 end
+
+def now_serving(deli)
+  if deli.length == 0
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{deli.shift}."
+  end
+end
